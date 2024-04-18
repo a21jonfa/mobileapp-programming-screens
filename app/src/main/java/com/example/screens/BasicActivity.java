fefbe75f2dbec97e5a496dea9e6043fbf887a191
Jonas <jfand125@gmail.com>
2024-04-18 15:34:03 +0200

    Add EditView, button to send view string to basic where its diplayed in a TextView

--- app/src/main/java/com/example/screens/BasicActivity.java
+++ app/src/main/java/com/example/screens/BasicActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class BasicActivity extends AppCompatActivity {
 
+    Bundle extras;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic);
+        textView = findViewById(R.id.textView);
+        extras = getIntent().getExtras();
+        if (extras != null) {
+            String message = extras.getString("message");
+            textView.setText(message);
+        }
     }
 }
--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        editText = findViewById(R.id.editTextNumber);
+
 
         button.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, BasicActivity.class);
+            intent.putExtra("message", editText.getText().toString());
             startActivity(intent);
         });
-
     }
 }
